1b3ec3ad40fe5bc8c15ebf9278c8adffb2038467
Karan Anchan <kar.anchan02@gmail.com>
2024-06-30 10:43:14 +0530

    Added docstrings for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 import os
 
 def ensure_dir(directory):
+    
+    """
+    Ensure that the directory exists.
+
+    Args:
+        directory (str): Path to the directory.
+    """
+    
     if not os.path.exists(directory):
         os.makedirs(directory)
