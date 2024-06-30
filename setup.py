0bd2eb55efc561df5e7e8833cd4ef5949c4e1c20
Karan Anchan <kar.anchan02@gmail.com>
2024-06-30 10:27:45 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Abdomen3DSegmentation',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'monai-weekly',
+        'torch',
+        'matplotlib',
+    ],
+)
