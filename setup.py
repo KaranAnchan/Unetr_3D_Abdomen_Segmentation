428bf4f5c99f83e405bd02ae7e7a0f3865f60d99
Karan Anchan <kar.anchan02@gmail.com>
2024-06-30 11:00:22 +0530

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Abdomen3DSegmentation',
+    name='UnetrAbdomen3DSegmentation',
     version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
