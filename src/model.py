19c98b550346a64aec2a4ef6ff129dfb29e4f20f
Karan Anchan <kar.anchan02@gmail.com>
2024-06-30 10:55:52 +0530

    Refactored model.py

--- src/model.py
+++ src/model.py
@@ -1,19 +1,19 @@
 import torch
 from monai.networks.nets import UNETR
 
-def create_model():
+def get_model():
     
     """
-    Create and return a UNETR model instance.
-    
+    Create and return a UNETR model.
+
     Returns:
-        UNETR: A UNETR model instance.
+        torch.nn.Module: UNETR model for 3D segmentation.
     """
     
     model = UNETR(
         in_channels=1,
         out_channels=14,
-        img_size=(96, 96, 96),
+        img_size=(128, 128, 128),
         feature_size=16,
         hidden_size=768,
         mlp_dim=3072,
